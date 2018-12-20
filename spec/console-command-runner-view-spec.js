376d6723cac8a8fad9c05bef957526e16ff69001
Lucas Sadle <lucas.sadle@capstonec.com>
2018-12-20 11:47:52 -0600

    INIT

---
+++ spec/console-command-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ConsoleCommandRunnerView from '../lib/console-command-runner-view';
+
+describe('ConsoleCommandRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
